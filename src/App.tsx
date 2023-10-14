546513f64dddcd764411cff001d3e8ddbcbc2f3f
Kirill <kirill-garnov@mail.ru>
2023-10-14 20:34:55 +0300

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { TagCloudComponent } from './components';
 
 export const App = () => {
   return (
     <Routes>
-      <Route
-        path=""
-        element={
-          <Navigate
-            to={{
-              pathname: `/`,
-            }}
-          />
-        }
-      />
-      <Route path="/" element={<TagCloudComponent />} />
-
-      {/* Route for redirect unknown paths */}
-      <Route
-        path="*"
-        element={
-          <Navigate
-            to={{
-              pathname: `/`,
-            }}
-          />
-        }
-      />
+      <Route path="*" element={<TagCloudComponent />} />
     </Routes>
   );
 };
